a8094163100b0df599a31405de8f376f8a35c02f
Gianpinetto <marco05.borelli@gmail.com>
2022-09-06 15:38:48 +0200

    Controllo che dopo che Mosconi ha messo il suo brutto account sul mio computer funzioni ancora tutto. Piccolie migliorie e controlli

--- Borelli_GestionaleVacanze/Form1.Designer.cs
+++ Borelli_GestionaleVacanze/Form1.Designer.cs
@@ -112,7 +112,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "LOGIN V- 0.7.0";
+            this.Text = "LOGIN V- 0.7.2";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
